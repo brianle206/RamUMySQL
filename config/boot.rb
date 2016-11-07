86aac5d3f8b0030caa7e51198dd927cc7f6aa01c
Eric Stephenson <eric.stephenson@rammount.com>
2016-11-07 11:18:24 -0800

    Add badge img & json, config project to work with vagrant

--- config/boot.rb
+++ config/boot.rb
@@ -1,3 +1,14 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
+
+
+# http://stackoverflow.com/a/29562898/1973777
+require 'rails/commands/server'
+module Rails
+  class Server
+    def default_options
+      super.merge(Host:  '0.0.0.0', Port: 3000)
+    end
+  end
+end
