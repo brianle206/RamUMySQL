d45f8816d815f197034f204cda4a8c1907d6d6da
Brian Le <brianle206@gmail.com>
2016-11-09 14:48:43 -0800

    intro layout/course layout and a little ui

--- app/helpers/learn_helper.rb
+++ app/helpers/learn_helper.rb
@@ -15,4 +15,16 @@ module LearnHelper
 	    @lecture = Lecture.find(params)
 	    @status = Complete.where(user_id: current_user.id, lecture_id: @lecture.id)
   	end
+
+ 	def resource_name
+    	:user
+  	end
+
+  	def resource
+    	@resource ||= User.new
+  	end
+
+  	def devise_mapping
+	    @devise_mapping ||= Devise.mappings[:user]
+  	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { registrations: "registrations" }
   resources :articles, :lecture, :admin, :dashboard, :learn, :question, :quiz, :data
 
   #Progress Tracker
