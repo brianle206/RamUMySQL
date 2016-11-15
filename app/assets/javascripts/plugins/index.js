acaddb85b76e7aa4d575e635da579320dfd0aa5d
Brian Le <brianle206@gmail.com>
2016-11-15 13:22:47 -0800

    image added to /courses

--- app/assets/javascripts/plugins/index.js
+++ app/assets/javascripts/plugins/index.js
@@ -1,5 +1,5 @@
 $('document').ready(function() {
-	var options = { videoId: 'F3vRd30WH5Q', start: 3, mute: false,volumeUpClass: 'tubular-volume-up' };
+	var options = { videoId: 'KjFpIpYNPEY', start: 3, mute: false,volumeUpClass: 'tubular-volume-up' };
 	$('#video').tubular(options);
 	// f-UGhWj1xww cool sepia hd
 	// 49SKbS7Xwf4 beautiful barn sepia
