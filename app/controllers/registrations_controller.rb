b010676b5072e4de49cedd57c3723b56e6265e62
Brian Le <brianle206@gmail.com>
2016-11-15 08:45:44 -0800

    Ordering

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,6 +2,6 @@ class RegistrationsController < Devise::RegistrationsController
   protected
 
   def after_sign_up_path_for(resource)
-    '/courses' # Or :prefix_to_your_route
+    '/courses' # Redirect after devise sign-up
   end
 end
--- app/models/learn.rb
+++ app/models/learn.rb
@@ -2,4 +2,5 @@ class Learn < ActiveRecord::Base
 	belongs_to :courses
 	has_many :lectures
 	has_many :quizzes, dependent: :destroy
+	default_scope { order(id: :asc) }
 end
