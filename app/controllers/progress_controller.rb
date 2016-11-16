7efd5cf8936f1038b40ac6c0b6419c45d4fd6371
Brian Le <brianle206@gmail.com>
2016-11-16 13:46:20 -0800

    lets hope nothing breaks

--- app/controllers/progress_controller.rb
+++ app/controllers/progress_controller.rb
@@ -5,4 +5,9 @@ class ProgressController < ApplicationController
   		redirect_to :back
   	end
   end
+  private 
+
+  def complete_params
+  	params.require(:completes).permit(:user_id, :lecture_id, :learn_id, :status => true)
+  end
 end
