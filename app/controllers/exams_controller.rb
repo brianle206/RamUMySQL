580ee261ca867f2965fc4b6a989461252995aaad
Eric Stephenson <eric.stephenson@rammount.com>
2016-11-21 11:16:37 -0800

    Refactor Exams Controller

--- app/controllers/exams_controller.rb
+++ app/controllers/exams_controller.rb
@@ -30,44 +30,19 @@ class ExamsController < ApplicationController
   end
 
   def destroy
+    if @exam.destroy
+      redirect_to exams_path
+      @notice = 'Exam was successfully deleted!'
+    end
   end
 
   def create_user_answer
     @course = Course.find_by(params[:course_id])
 
-    grading = params[:answer]
-    @correct = 0
-    grading.each do |grading|
-      find_question(grading[0])
-      find_answer(grading[1])
-      if @answer == @question.answer
-        @correct += 1
-      end
-    end
-    puts "Total correct answers: #{@correct}"
+    grade_exam
 
-    record = ((@correct.to_f/params[:answer].count)*100).round
+    check_exam_attempts
 
-    if check_for_exam(current_user.id, @exam)
-      if @users_exam.score < record
-        @users_exam.update(score: record)
-        @users_exam.attempts += 1
-        @users_exam.save
-      else
-        @users_exam.attempts += 1
-        @users_exam.save
-      end
-    elsif @users_exam.blank?
-      @score = UserExamResult.new(exam_id: @exam, user_id: current_user.id, score: record)
-      if @score.save
-        @success = "You successfully submitted your exam!"
-        update_attempt
-      else
-        @success = "Uh Oh! something went wrong"
-      end
-    end
-
-    # Assertion generator
     generate_assertion
   end
 
@@ -98,21 +73,56 @@ class ExamsController < ApplicationController
     @users_exam =  UserExamResult.find_by(exam_id: exam.to_i, user_id: user)
   end
 
+  def check_exam_attempts
+    if check_for_exam(current_user.id, @exam)
+      if @users_exam.score < record
+        @users_exam.update(score: record)
+        @users_exam.attempts += 1
+        @users_exam.save
+      else
+        @users_exam.attempts += 1
+        @users_exam.save
+      end
+    elsif @users_exam.blank?
+      @score = UserExamResult.new(exam_id: @exam, user_id: current_user.id, score: record)
+      if @score.save
+        @notice = "You successfully submitted your exam!"
+        update_attempt
+      else
+        @notice = "Uh Oh! something went wrong"
+      end
+    end
+  end
+
+  def grade_exam
+    grading = params[:answer]
+    @correct = 0
+    grading.each do |grading|
+      find_question(grading[0])
+      find_answer(grading[1])
+      if @answer == @question.answer
+        @correct += 1
+      end
+    end
+    puts "Total correct answers: #{@correct}"
+    record = ((@correct.to_f/params[:answer].count)*100).round
+  end
+
   def generate_assertion
     puts @score.score
     if @score.score >= 85.00
       begin
         puts "START UP THE GENERATOR!!!!"
         recipient = { type: "email", identity: current_user.email, hashed: false }
-        puts recipient
+        puts "Recipient: #{recipient}"
         @badge = Badge.find_by(course_id: @course)
-        puts @badge.id
+        puts "Badge ID: #{@badge.id}"
         badge = "http://frozen-dawn-78535.herokuapp.com/badges/#{@badge.id}"
-        puts badge
+        puts "Badge: #{badge}"
         @assertion = Assertion.new(user_id: current_user.id, badge_id: @badge.id, recipient: recipient, badge: badge, issued_on: DateTime.now, expires: DateTime.now + 2.years)
-        puts @assertion.to_json
+        puts "Assertion: #{@assertion.to_json}"
         @assertion[:verify] = { type: "hosted", url: "http://frozen-dawn-78535.herokuapp.com/assertions/#{@assertion.id}" }
-        puts @assertion.to_json
+        puts "Assertion with verify: #{@assertion.to_json}"
       rescue => err
         Rails.logger.error "Womp womp, no assertion for you!"
         Rails.logger.error "#{err.message}\n#{err.backtrace.join("\n")}"
