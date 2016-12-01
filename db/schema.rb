b0af30e67b8d175f6b18d39941778fcb32aa0d2b
Eric Stephenson <eric.stephenson@rammount.com>
2016-12-01 11:04:48 -0800

    Attempt to show hidden templates in mozilla open badges modal

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161128224522) do
+ActiveRecord::Schema.define(version: 20161130224622) do
 
   create_table "answers", force: :cascade do |t|
     t.integer  "question_id", limit: 4
