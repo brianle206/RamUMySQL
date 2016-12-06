b3fad32d194b7481c4f6ff6cfebbc13ce608385e
Brian Le <brianle206@gmail.com>
2016-12-06 13:39:00 -0800

    complete

--- app/assets/javascripts/pagination.js
+++ app/assets/javascripts/pagination.js
@@ -1,5 +1,5 @@
 $(function() {
 	$('ul > li.next').click(function(){
-		$('#new_complete').trigger('submit.rails')
-	})
+		$('#new_complete').trigger('submit.rails');
+	});
 });
--- app/controllers/progress_controller.rb
+++ app/controllers/progress_controller.rb
@@ -6,6 +6,7 @@ class ProgressController < ApplicationController
   	@progress = Complete.create(user_id: params[:complete][:user_id], lecture_id: params[:complete][:lecture_id], status: params[:complete][:status], learn_id: params[:complete][:learn_id])
   	if @progress.save
       puts "It Saved"
+      flash[:notice] = "Congrats on completing that lecture!!"
   	end
   end
   private 
